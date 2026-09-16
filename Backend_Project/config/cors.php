7dc92436fe60ccce42bfcc9eed08cb31fe4cf35e
adialex30 <dwiirawana@gmail.com>
2026-09-16 22:11:36 +0700

    update config

--- Backend_Project/config/cors.php
+++ Backend_Project/config/cors.php
@@ -19,10 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'https://mini-project-react-taupe.vercel.app',
-        'http://localhost:5173',
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
@@ -32,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
